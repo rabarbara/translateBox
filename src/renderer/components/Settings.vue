bed160fff2143d3b58e97975273d5da5be552798
Matej Stavanja Laptop <stavanja@gmail.com>
2017-07-07 16:13:41 +0200

    add settings file for api keys

--- src/renderer/components/Settings.vue
+++ src/renderer/components/Settings.vue
@@ -2,31 +2,53 @@
   <div id="settings" class="input-form">
     <div class="input-form-container">
       <div>
-        <h1>Dictionary key</h1>
-      <input type="text" v-model="apikey" placeholder="Paste your api key here">
+        <h1 class="settings_heading">Dictionary keys</h1>
+      <input type="text" v-model="apiKey" placeholder="Paste your api key here" @keypress.enter="inputApiKey()">
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'settings',
-    // components: { Explanations },
-    data () {
-      return {
-        apikey: ''
-      }
+import electron from 'electron'
+export default {
+  name: 'settings',
+  // components: { Explanations },
+  data () {
+    return {
+      apiKey: '',
+      print: ''
+    }
+  },
+  computed: {
+  },
+  methods: {
+    inputApiKey () {
+      let settings = electron.remote.require('electron-settings')
+      let apiKey = this.apiKey
+      settings.set('medical', apiKey)
     },
-    computed: {
+    displays () {
+      let settings = electron.remote.require('electron-settings')
+      this.print = settings.get('medical')
     }
   }
+}
 </script>
 
 <style lang="scss">
+
   $main-color: white;
   $background-color: rgb(117, 169, 255);
 
+
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body { font-family: 'Source Sans Pro', sans-serif; background: $background-color;}
   .input-form {
     position:absolute;
     top: 0;
@@ -37,10 +59,14 @@
   }
   .input-form-container {
     display: flex;
-    justify-content: center;
-    align-items: center;
+    justify-content: flex-start;
+    // align-items: center;
     height: 100%;
 
+    & > div {
+      margin-top: 1em;
+      margin-left: 2em;
+    }
     input {
       background-color: $background-color;
       border: 3px solid $main-color;
@@ -48,6 +74,11 @@
       color: $main-color;
       font-size: 1em;
     }
+  }
+  .settings_heading {
+    color: $main-color;
+    margin-bottom: .5em;
+
   }
 
 </style>
