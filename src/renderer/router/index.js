7e7adf55b57b790b397066bd2a0d885db83a3114
matej <stavanja@gmail.com>
2017-07-12 17:22:41 +0200

    rearrange the routes to exclude the starter component

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -7,22 +7,17 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage')
-    },
-    {
-      path: '/main',
       name: 'translator-box',
       component: require('@/components/TranslatorBox')
-    },
-    {
-      path: '*',
-      redirect: '/'
     },
     {
       path: '/settings',
       name: 'settings',
       component: require('@/components/Settings')
+    },
+    {
+      path: '*',
+      redirect: '/'
     }
   ]
 })
