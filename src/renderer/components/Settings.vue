642318ac833d4678d2d2139b064d0e5d46cd158e
matej <stavanja@gmail.com>
2017-08-06 21:28:00 +0200

    remove animation from settings

--- src/renderer/components/Settings.vue
+++ src/renderer/components/Settings.vue
@@ -1,18 +1,23 @@
 <template>
-  <div id="settings" class="input-form">
-    <div class="input-form-container ">
-      <div class="animated slideInLeft">
-        <h1 class="settings_heading">Dictionary keys</h1>
-      
-      <div class="input-elements">
-        <select name="collegiate" id="collegiate">
-          <option value="md">Medical</option>
-          <option value="col">Collegiate</option>
-        </select>
-        <input type="text" v-model="apiKey" placeholder="Paste your api key here" @keypress.enter="inputApiKey()">
-      </div>
+  <div id="settings" >
+    <div class="input-form">
+    <div class="input-form-container">
+      <div class="">
+        <h1 class="settings_heading">Settings</h1>
+        <div class="input-elements">
+          <select name="collegiate" id="collegiate">
+            <option value="md">Medical</option>
+            <option value="col">Collegiate</option>
+          </select>
+          <input type="text" v-model="apiKey" placeholder="Paste your api key here" @keypress.enter="inputApiKey()">
+        </div>
       </div>
     </div>
+</div>
+    <button class="lookup_button">
+      <router-link to="/">Back</router-link>
+    </button>
+
   </div>
 </template>
 
@@ -20,7 +25,6 @@
 import electron from 'electron'
 export default {
   name: 'settings',
-  // components: { Explanations },
   data () {
     return {
       apiKey: '',
@@ -43,56 +47,76 @@ export default {
 }
 </script>
 
-<style lang="scss">
-
-  $main-color: white;
-  $background-color: rgb(117, 169, 255);
-  $alt-color: rgb(50, 50, 50);
-@import url('https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css');
+<style lang="scss" scoped>
+$main-color: white;
+$background-color: rgb(117, 169, 255);
+$alt-color: rgb(50, 50, 50);
+// @import url('https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css');
 @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Source Sans Pro', sans-serif;
+  background: $background-color;
+}
+
+.input-form {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: $background-color;
+}
+
+.input-form-container {
+  display: flex;
+  justify-content: flex-start; // align-items: center;
+  &>div {
+    margin-top: 1em;
+    margin-left: 2em;
   }
-  body { font-family: 'Source Sans Pro', sans-serif; background: $background-color;}
-  .input-form {
-    position:absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
+  input {
     background-color: $background-color;
-  }
-  .input-form-container {
-    display: flex;
-    justify-content: flex-start;
-    // align-items: center;
-    height: 100%;
-
-    & > div {
-      margin-top: 1em;
-      margin-left: 2em;
-    }
-    input {
-      background-color: $background-color;
-      border: 3px solid $main-color;
-      padding: 1em;
-      color: $main-color;
-      font-size: 1em;
-    }
-  }
-  .settings_heading {
+    border: 3px solid $main-color;
+    padding: 1em;
     color: $main-color;
-    margin-bottom: .5em;
+    font-size: 1em;
   }
-  select {
-    background: $main-color;
-    border: 3px solid $alt-color;
-    color: $alt-color;
-    padding: .5em;
-    font-size: .8em;
-    font-weight: bold;
-  }
+}
 
+.settings_heading {
+  color: $main-color;
+  margin-bottom: .5em;
+}
+
+select {
+  background: $main-color;
+  border: 3px solid $alt-color;
+  color: $alt-color;
+  padding: .5em;
+  font-size: .8em;
+  font-weight: bold;
+}
+
+.lookup_button {
+  font-weight: bold;
+  border: 3px solid $main-color;
+  background: rgba(255, 255, 255, 0);
+
+  text-transform: uppercase;
+  color: $main-color;
+  cursor: pointer;
+  padding: 1em;
+  font-size: 1em;
+
+  a {
+    color: $main-color;
+    text-decoration: none;
+  }
+}
 </style>
